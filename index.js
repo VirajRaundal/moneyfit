6bd47224c6441e625b71e63b9cfac9cb77a0ef2f
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-11-04 15:05:19 +0530

    mongo connection established

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.use(express.json())
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening to port ${port}`))
 
+const dbURi = process.env.DATABASE;
+
+mongoose.connect(dbURi)
+  .then(() => console.log(`Connected to database...`))
+  .catch(err => console.log(`Error encountered while connecting to db: ${err}`))
+
 app.get("/", (req, res) => {
   res.send("Redirect to /xyz")
 })
