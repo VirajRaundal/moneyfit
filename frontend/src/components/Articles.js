9734556594cc4546e65369811a1b381a5e606002
GARlMAN <garimanjohal@gmail.com>
2022-11-10 03:28:48 +0530

    Education-page-v2

--- frontend/src/components/Articles.js
+++ frontend/src/components/Articles.js
@@ -3,15 +3,9 @@ import "./Articles.css";
 function Articles(props) {
   return (
     <div>
-        <h2 className='edu-card-title'>1. Introduction to Stock Markets</h2>
-        <h3 className='edu-card-chapters'>15 chapters</h3>
-        <p className='edu-card-info'>Investing ensures financial security, 
-        and the Stock market plays a pivotal role in 
-        this domain, it is a place where people buy/sell 
-        shares of publicly listed companies. In this module, 
-        you will learn about the fundamentals of the stock market, 
-        how to get started, how it functions and the various intermediaries 
-        that appertain it.</p>
+        <h2 className='edu-card-title'>{props.title}</h2>
+        <h3 className='edu-card-chapters'>{props.chapters}</h3>
+        <p className='edu-card-info'>{props.info}</p>
 
 
     </div>
