eb6f7b7d90350998f1375cff95fad295a0d4ba00
GARlMAN <garimanjohal@gmail.com>
2022-11-10 23:00:49 +0530

    updates footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "../styles/components/Footer.css";
+import {FaInstagram, FaTwitter, FaFacebook} from "react-icons/fa"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className = "footer">
+      <div>
+        <p className = 'footer-quote'>Ending Quote</p>
+        <p className ='creaters'>Made with <span className ="footer-heart">❤</span> by Pandavs</p>
+        <p className = 'footer-contact'>Need Help? Contact us!</p>
+        <div className ='footer-icons'>
+          <div className ='icon-center'>
+            <FaInstagram size={24}/>
+            <FaTwitter size={24}/>
+            <FaFacebook size={24}/>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
