bf843fe2d2ed45e23da4eafebb3b50c3ddbcf471
Rijul Sarji <64856177+rijulsarji@users.noreply.github.com>
2022-11-11 23:17:15 +0530

    payment tracker intiated

--- frontend/src/pages/PaymentTracker.js
+++ frontend/src/pages/PaymentTracker.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 const PaymentTracker = () => {
+
+  const date = new Date();
+  
+  const result2 = date.toLocaleTimeString("en-GB", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  })
+  console.log(result2)
+
+  const result = date.toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+  console.log(result)
+
   return (
     <div>PaymentTracker</div>
   )
